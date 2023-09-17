04b8fbc8ed2d115d03d902392e8857fd10f2f5d8
D0000ds <93730587+D0000ds@users.noreply.github.com>
2023-09-18 00:26:52 +0200

    Delete forumPlateau directory

--- forumPlateau/view/security/login.php
+++
@@ -1,8 +0,0 @@
-
-<h1>Connexion</h1>
-<form action="../../index.php?ctrl=security&action=login" method="post">
-    <input type="text" name="username" placeholder="Username" autocomplete="off" required/>
-    <input type="password" name="password" placeholder="Password" autocomplete="off" required/>
-    <input type="submit" value="Login" name="login"/>
-</form>
-
